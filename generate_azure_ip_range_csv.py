7a22f3cde4a730c96c2152bb0b0fcc77e0cebc10
newtonapple1987 <nabmlbcpbl@gmail.com>
2023-05-15 17:06:49 +0800

    add quote

---
+++ generate_azure_ip_range_csv.py
@@ -0,0 +1,15 @@
+import json
+
+data = open("ServiceTags_Public_20230508.json")
+data = data.read()
+data = json.loads(data)
+
+output = open("azure_ip_range.csv", "w")
+output.write('"ip","name",""id","region","platform","systemService"\n')
+
+for i in data["values"]:
+    for  j in i["properties"]["addressPrefixes"]:
+        output.write('"')
+        output.write('","'.join([j,i["name"],i["id"],i["properties"]["region"],i["properties"]["platform"],i["properties"]["systemService"]]))
+        output.write('"\n')
+output.close()
